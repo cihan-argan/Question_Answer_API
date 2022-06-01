4b5e25838dba7a78595b445dc725dcef9c138d7b
Cihan Argan <cihan.argan@outlook.com>
2022-06-01 16:49:10 +0300

    add to new User -Register

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,21 @@
 //Controller>auth.js
-const register = (req, res, next) => {
+const User = require('../models/User');
+
+const register = async (req, res, next) => {
+	//POST DATA gelecek fakat test amaçlı biz verileri burda oluşturucaz.
+	const name = 'Sezai Argan';
+	const email = 'dalokay68@outlook.com';
+	const password = '123456';
+	//zorunlu verilmesi gereken bilgileri hazırladık ve yeni kullanıcı oluşturabileceğiz bunun için async await yapısını kullanacağız.ilk başta register fonksiyonunu async e çevirmemiz gerekiyor ki içinde await yapısını kullanabilelim
+	const user = await User.create({
+		name, //name : name,
+		email, //email : email,
+		password //password :password ES6 Standartları gereği bu şekilde vermeye gerek kalmadı.bu veriler beklenecek bir hata çıkmaz ise yani validation hatası çıkmaz ise verilerimiz gelecek bizde bunu const user olarak alabileceğiz.
+	});
+	//Not: Postman üzerinden registera post yapınca fonksiyonumuz çalışacak ve userımız oluşacak oluşan user bize geri dönecek.
 	res.status(200).json({
-		success: true
+		success: true,
+		data: user
 	});
 };
 module.exports = {
