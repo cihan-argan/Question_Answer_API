c82a86fd041fe5e922f8564ae8fd07c41b96e22e
Cihan Argan <cihan.argan@outlook.com>
2022-06-17 17:09:15 +0300

    Delete A User By Admin

--- controllers/admin.js
+++ controllers/admin.js
@@ -6,11 +6,21 @@ const blockUser = asyncErrorWrapper(async (req, res, next) => {
 	const { id } = req.params; //id yi paramstan alıyoruz
 	const user = await User.findById(id); //id ye ait kullanıcıyı buluyoruz
 	user.bloked = !user.bloked; //bloked true ise false false ise true yapıyoruz.
-    
-	await user.save();//işlem gerçekleştikten sonra kullanıcıyı kayıt ediyoruz.
+
+	await user.save(); //işlem gerçekleştikten sonra kullanıcıyı kayıt ediyoruz.
 	return res.status(200).json({
 		success: true,
 		message: 'Block-Unblock Successfull'
 	});
 });
-module.exports = { blockUser };
+const deleteUser = asyncErrorWrapper(async (req, res, next) => {
+	const { id } = req.params;
+	const user = await User.findById(id);
+	await user.remove(); //db den ilgili kullanıcıyı silmiş olacağız.
+	return res.status(200).json({
+		success: true,
+		message: 'Delete Operation Successfull'
+	});
+});
+
+module.exports = { blockUser, deleteUser };
--- routers/admin.js
+++ routers/admin.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 const { getAccessToRoute, getAdminAccess } = require('../middlewares/authorization/auth');
 const { checkUserExist } = require('../middlewares/database/databaseErrorHandler');
-const { blockUser } = require('../controllers/admin'); //Block User
+const { blockUser, deleteUser } = require('../controllers/admin'); //Block User
 //delete User
 
 router.use([ getAccessToRoute, getAdminAccess ]); //Bu artık tüm routelarda geçerli olacak ilk önce getAccessToRoute çalışacak sonra getAdminAccess Çalışacak.
 
 router.get('/block/:id', checkUserExist, blockUser);
+router.delete('/user/:id', checkUserExist, deleteUser);
 
 module.exports = router;
